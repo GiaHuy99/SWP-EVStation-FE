c858d38091f8c2f329eed438e9f8ca20fc4e27ef
htonggg <htong3103@gmail.com>
2025-10-05 19:51:40 +0700

    GetbyId,Update,Delete

--- src/features/vehicle/VehicleSlices.ts
+++ src/features/vehicle/VehicleSlices.ts
@@ -32,18 +32,6 @@ export const vehicleSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload as string;
             })
-            .addCase(fetchVehicles.pending, (state) => {
-                state.loading = true;
-                state.error = null;
-            })
-            .addCase(fetchVehicles.fulfilled, (state, action) => {
-                state.loading = false;
-                state.vehicles = action.payload;
-            })
-            .addCase(fetchVehicles.rejected, (state, action) => {
-                state.loading = false;
-                state.error = action.payload as string;
-            })
 
             // FETCH ALL
             .addCase(fetchVehicles.pending, (state) => {
