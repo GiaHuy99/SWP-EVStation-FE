dccdae7de564c0551b6d28cac9d9cacb639061e2
huy1412 <huytgse170167@fpt.edu.vn>
2025-11-27 03:44:20 +0700

    done v:4.1

--- src/features/profileUser/services/profileService.ts
+++ src/features/profileUser/services/profileService.ts
@@ -1,13 +1,30 @@
 // src/features/profile/services/profileService.ts
 import axiosInstance from "../../../shared/utils/AxiosInstance";
-import { UserProfile, UpdateProfileRequest } from "../types/ProfileType";
+import {UserProfile, UpdateProfileRequest, BatteryDetail} from "../types/ProfileType";
+import { VehicleDetail} from "../types/ProfileType";
 
 export const fetchUserProfileApi = async (): Promise<UserProfile> => {
     const res = await axiosInstance.get("/user/profile");
     return res.data;
 };
+interface VehicleApiResponse {
+    vehicleId: number;
+    vin: string;
+    planName: string;
+    batteries: BatteryDetail[];
+}
 
 export const updateUserProfileApi = async (data: UpdateProfileRequest): Promise<UserProfile> => {
     const res = await axiosInstance.put("/user/profile", data);
     return res.data;
 };
+export async function getAllVehicles(): Promise<VehicleDetail[]> {
+    const res = await axiosInstance.get<VehicleApiResponse[]>("/user/vehicles");
+
+    return res.data.map((vehicle) => ({
+        id: vehicle.vehicleId,
+        vehicleName: vehicle.vin,
+        currentPlan: vehicle.planName,
+        batteries: vehicle.batteries || [], // nếu backend trả về null/undefined
+    }));
+}
